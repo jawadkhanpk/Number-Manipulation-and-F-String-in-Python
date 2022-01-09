5bad2dfb621ae19b5d6dcdb22e101daf67b91f68
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-09 18:34:26 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# print(8/3)
+# print(round(8/3))
+# print(8 // 3)
+#
+# result = 4/2
+# result /= 2     #result = result / 2
+# print(result)
+#
+# score = 0
+# score += 1
+# score -= 1
+# score *= 1
+# score /= 1
+#
+# print(score)
+
+
+score = 0
+height = 1.8
+isWinning = True
+
+#F-String
+print(f"your score is {score}, \n your height is {height}, \n you are winning is: {isWinning}")
